4d54d2e7384f0b61d250e2a82873036c5c1a15ad
Sobir <sabdullaev@avanpost.ru>
2023-08-03 17:45:15 +0300

    добавлена модель Собака

--- dogs/models.py
+++ dogs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Dog(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Кличка')
+    category = models.CharField(max_length=100, verbose_name='Порода')
+    phot = models.ImageField(upload_to='dogs/', null=True, blank=True, verbose_name='Фото')
+    birth_day = models.DateField(null=True, blank=True, verbose_name='Дата рождения')
+
+    def __str__(self):
+        return f'{self.name} ({self.category})'
+
+    class Meta:
+        verbose_name = 'собака'
+        verbose_name_plural = 'собаки'
