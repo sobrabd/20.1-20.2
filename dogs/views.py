5c5eb8b8b0f7f7aedadd8ac94a77fc5315f0869c
Sobir <sabdullaev@avanpost.ru>
2023-08-04 12:25:29 +0300

    добавил вывод пород на главной странице

--- dogs/views.py
+++ dogs/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Category
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'dogs/base.html')
+    context = {
+        'objects_list': Category.objects.all(),
+        'title': 'Питомник - Наши породы'
+    }
+
+    return render(request, 'dogs/base.html', context)
+
