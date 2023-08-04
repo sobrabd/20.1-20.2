1fca90c52d2fab1eed4a353a7f06058b146cfe24
Sobir <sabdullaev@avanpost.ru>
2023-08-04 13:21:58 +0300

    Добавлена страница со всеми породами

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
+from .apps import DogsConfig
 from . import views
+
+app_name = DogsConfig.name
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("categories/", views.categories, name="categories"),
 ]
--- dogs/views.py
+++ dogs/views.py
@@ -5,9 +5,16 @@ from .models import Category
 # Create your views here.
 def index(request):
     context = {
-        'objects_list': Category.objects.all(),
+        'objects_list': Category.objects.all()[:3],
         'title': 'Питомник - Наши породы'
     }
 
     return render(request, 'dogs/base.html', context)
 
+def categories(request):
+    context = {
+        'objects_list': Category.objects.all(),
+        'title': 'Питомник - Наши породы'
+    }
+
+    return render(request, 'dogs/base.html', context)
